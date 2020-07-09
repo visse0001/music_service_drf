dfefbc43e7881fb59e6a088019436a967fe431b7
Sandra_Kuczynska <san.kuczynska@gmail.com>
2020-07-09 09:27:38 +0200

    add rest framework versioning class to settings

--- music_service_drf/settings.py
+++ music_service_drf/settings.py
@@ -115,3 +115,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+REST_FRAMEWORK = {
+    'DEFAULT_VERSIONING_CLASS': 'rest_framework.versioning.URLPathVersioning',
+}
