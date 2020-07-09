ddfee03c50a8f5fafd854fa30df45c24273f3dfb
Sandra_Kuczynska <san.kuczynska@gmail.com>
2020-07-09 09:26:56 +0200

    models

--- music/models.py
+++ music/models.py
@@ -1,8 +1,9 @@
 from django.db import models
 
+
 class Songs(models.Model):
     title = models.CharField(max_length=255, null=False)
     artist = models.CharField(max_length=255, null=False)
 
     def __str__(self):
-        return f'{self.title} - {self.artist}'
+        return f"{self.title} - {self.artist}"
